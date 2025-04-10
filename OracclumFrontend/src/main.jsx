66de6d393de4a7977eb25a5529e93ebe4a7707e2
Victor Z <victormsz09@yahoo.com.br>
2025-04-09 23:02:36 -0300

    bugfix

--- OracclumFrontend/src/main.jsx
+++ OracclumFrontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import QuestionApp from './Pages/Questions/QuestionApp'
-import LoginApp from './Pages/Login/loginApp'
+import LoginApp from './Pages/Login/LoginApp'
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
