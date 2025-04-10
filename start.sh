fee4e76a8c42a9c9d9d3877342027fad22e61b18
Victor Z <victormsz09@yahoo.com.br>
2025-04-10 15:32:53 -0300

    Feat: added and fixed html localhost opening

--- start.sh
+++ start.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
-
 set -e
 
-echo "🧩 Iniciando backend Node.js..."
-
+echo "Iniciando backend Node.js..."
 cd Backend_Node
 npm install
-
-# Altere para 'npm start' se não tiver script 'dev' no package.json
-npm run dev &  
+npm run start &
 BACKEND_PID=$!
-
 cd ..
 
-echo "⚛️ Iniciando frontend React..."
-
+echo "Iniciando frontend React..."
 cd Frontend_React
 npm install
-npm run dev
+npm run dev &
+FRONTEND_PID=$!
+cd ..
 
-# Finaliza backend quando React for parado com Ctrl+C
-kill $BACKEND_PID
+echo "Servindo Frontend HTML estático..."
+npx serve Frontend_html/Index -l 5500 &
+HTML_PID=$!
+
+sleep 2
+open http://localhost:5500/Index
+
+wait $FRONTEND_PID
+
+kill $BACKEND_PID $HTML_PID
